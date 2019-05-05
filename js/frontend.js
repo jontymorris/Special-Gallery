d44e56688daba383f6dba60c72fc51c3a207b182
Jonty <jonty.morris@yahoo.co.nz>
2019-05-05 14:44:57 +1200

    Worked on frontend

--- js/frontend.js
+++ js/frontend.js
@@ -2,9 +2,16 @@ const galleryApp = new Vue({
     'el': '#galleryApp',
     'data': {
         items: [],
-        thumbnails: {}
+        thumbnails: {},
+        fullsize: {},
+        selected: null
     },
     'methods': {
-        
+        itemClick: function(item) {
+            this.selected = item;
+        },
+        close: function() {
+            this.selected = null;
+        }
     }
 });
--- views/embed.php
+++ views/embed.php
@@ -1,19 +1,18 @@
 <?php
-    function get_thumbnail($id) {
-        return wp_get_attachment_image_src( $id, 'thumbnail' )[0];
-    }
-
     $data =  file_get_contents( plugin_dir_path( __FILE__ ) . '../data/items.json' );
     $items = json_decode( $data );
 
     $thumbnails = array();
+    $fullsize = array();
     foreach ($items as $item) {
         foreach ($item->images as $image) {
-            if ( array_key_exists( $image, $thumbnails) ) {
-                continue;
+            if ( !array_key_exists( $image, $thumbnails) ) {
+                $thumbnails[$image] = wp_get_attachment_image_src( $image, 'thumbnail' )[0];
             }
 
-            $thumbnails[$image] = get_thumbnail( $image );
+            if ( !array_key_exists( $image, $fullsize) ) {
+                $fullsize[$image] = wp_get_attachment_image_src( $image, 'full' )[0];
+            }
         }
     }
 ?>
@@ -22,7 +21,20 @@
 
     <div class="special-gallery">
         <div class="thumbnail" v-for="item in items">
-            <img :src="thumbnails[item.images[0]]">
+            <img :src="thumbnails[item.images[0]]" v-on:click="itemClick(item)">
+        </div>
+    </div>
+
+    <div id="myModal" class="modal" v-if="selected">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close" v-on:click="close">&times;</span>
+            <div class="image-container">
+                <img :src="fullsize[selected.images[0]]">
+            </div>
+            <span class="blurb">
+                {{ selected.blurb }}
+            </span>
         </div>
     </div>
 
@@ -36,6 +48,8 @@
 <script src="<?php echo plugins_url( 'special-gallery/js/frontend.js' ) ?>"></script>
 
 <script>
+    // load the gallery
     galleryApp.thumbnails = JSON.parse('<?php echo json_encode( $thumbnails ) ?>');
+    galleryApp.fullsize = JSON.parse('<?php echo json_encode( $fullsize ) ?>');
     galleryApp.items = JSON.parse('<?php echo $data; ?>');
 </script>
